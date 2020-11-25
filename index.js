83946ebc2004705d421833edacb76300412d2bf7
pabloferreri <pab.ferreri@gmail.com>
2020-11-25 00:21:02 -0300

    funcion listaDeVentas en concesionario

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let concesionaria = {
   
     buscarAuto:  function(patente){
         return this.autos.find(auto => auto.patente == patente)||null;
-    }
+    },
     venderAuto: function(patente){
         let resultado = this.buscarAuto(patente);
         if (resultado){
@@ -14,17 +14,25 @@ let concesionaria = {
     },
     autosParaLaVenta: function (){
         return autos.filter(auto => auto.vendido == false)
-    }
+    },
     autos0KM : function () {
         let resultado = this.autosParaLaVenta()
         
         return resultado.filter(auto => auto.km < 100)
+    },
+    listaDeVentas : function (){
+        let autosVendidos = autos.filter(elemento => elemento.vendido == true) 
+        let precioVendidos = autosVendidos.map(function (autosVendidos) {
+            return autosVendidos.precio
+        })
+        return precioVendidos
     }
 }
 
 /*console.log("Auto Vendido")*/
 
 concesionaria.venderAuto("APL123")
+concesionaria.venderAuto("JJK116")
 
 console.log("AUTOS PARA LA VENTA----------------------------------------------------")
 
@@ -33,6 +41,10 @@ console.log(concesionaria.autosParaLaVenta())
 console.log('AUTOS 0KM------------------------------------------------------------')
 
 console.log(concesionaria.autos0KM());
+
+console.log("Lista de precio de los autos vendidos ----------------------------------")
+
+console.log(concesionaria.listaDeVentas())
 
 console.log('AUTOS')
 
